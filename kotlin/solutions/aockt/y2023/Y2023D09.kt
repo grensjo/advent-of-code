30d198b3f5703df6880af7acb1a3649884e5026a
Anton Grensjö <anton@grensjo.se>
2023-12-09 09:09:32 +0100

    Day 9: cleanup.

--- kotlin/solutions/aockt/y2023/Y2023D09.kt
+++ kotlin/solutions/aockt/y2023/Y2023D09.kt
@@ -4,7 +4,7 @@ import io.github.jadarma.aockt.core.Solution
 
 private data class NumberSequence(val list: List<Int>) {
     private fun getDiffSequence() =
-        NumberSequence(list.zipWithNext().map { (a, b) -> b - a }).also { println(it) }
+        NumberSequence(list.zipWithNext().map { (a, b) -> b - a })
 
     fun predictNext() : Int = if (list.all { it == 0 }) 0 else list.last() + getDiffSequence().predictNext()
     fun predictPrevious() : Int = if (list.all { it == 0 }) 0 else list.first() - getDiffSequence().predictPrevious()
@@ -13,10 +13,9 @@ private data class NumberSequence(val list: List<Int>) {
 private fun String.toNumberSequence() = NumberSequence(trim().split(' ').map(String::toInt))
 
 object Y2023D09 : Solution {
-
     override fun partOne(input: String): Int =
-        input.lineSequence().map(String::toNumberSequence).onEach { println(it) }.map(NumberSequence::predictNext).onEach { println(it) }.sum().also { println(it) }
+        input.lineSequence().map(String::toNumberSequence).sumOf(NumberSequence::predictNext)
 
     override fun partTwo(input: String): Int =
-        input.lineSequence().map(String::toNumberSequence).onEach { println(it) }.map(NumberSequence::predictPrevious).onEach { println(it) }.sum().also { println(it) }
+        input.lineSequence().map(String::toNumberSequence).sumOf(NumberSequence::predictPrevious)
 }
