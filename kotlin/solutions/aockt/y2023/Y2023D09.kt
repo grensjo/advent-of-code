2326c820539b43c36bf1e3f1cd5ac55df01a3094
Anton Grensjö <anton@grensjo.se>
2023-12-09 09:20:34 +0100

    Day 9: style fix.

--- kotlin/solutions/aockt/y2023/Y2023D09.kt
+++ kotlin/solutions/aockt/y2023/Y2023D09.kt
@@ -3,11 +3,10 @@ package aockt.y2023
 import io.github.jadarma.aockt.core.Solution
 
 private data class NumberSequence(val list: List<Int>) {
-    private fun getDiffSequence() =
-        NumberSequence(list.zipWithNext().map { (a, b) -> b - a })
+    private fun getDiffSequence() = NumberSequence(list.zipWithNext().map { (a, b) -> b - a })
 
-    fun predictNext() : Int = if (list.all { it == 0 }) 0 else list.last() + getDiffSequence().predictNext()
-    fun predictPrevious() : Int = if (list.all { it == 0 }) 0 else list.first() - getDiffSequence().predictPrevious()
+    fun predictNext(): Int = if (list.all { it == 0 }) 0 else list.last() + getDiffSequence().predictNext()
+    fun predictPrevious(): Int = if (list.all { it == 0 }) 0 else list.first() - getDiffSequence().predictPrevious()
 }
 
 private fun String.toNumberSequence() = NumberSequence(trim().split(' ').map(String::toInt))
