3e5037a34c3f6dc39bb8d86c32544dc3b6daab95
Anton Grensjö <anton@grensjo.se>
2023-12-09 09:07:23 +0100

    Day 9 part 2.

--- kotlin/solutions/aockt/y2023/Y2023D09.kt
+++ kotlin/solutions/aockt/y2023/Y2023D09.kt
@@ -7,6 +7,7 @@ private data class NumberSequence(val list: List<Int>) {
         NumberSequence(list.zipWithNext().map { (a, b) -> b - a }).also { println(it) }
 
     fun predictNext() : Int = if (list.all { it == 0 }) 0 else list.last() + getDiffSequence().predictNext()
+    fun predictPrevious() : Int = if (list.all { it == 0 }) 0 else list.first() - getDiffSequence().predictPrevious()
 }
 
 private fun String.toNumberSequence() = NumberSequence(trim().split(' ').map(String::toInt))
@@ -16,7 +17,6 @@ object Y2023D09 : Solution {
     override fun partOne(input: String): Int =
         input.lineSequence().map(String::toNumberSequence).onEach { println(it) }.map(NumberSequence::predictNext).onEach { println(it) }.sum().also { println(it) }
 
-    override fun partTwo(input: String): Long {
-        return 0L
-    }
+    override fun partTwo(input: String): Int =
+        input.lineSequence().map(String::toNumberSequence).onEach { println(it) }.map(NumberSequence::predictPrevious).onEach { println(it) }.sum().also { println(it) }
 }
--- kotlin/tests/aockt/y2023/Y2023D09Test.kt
+++ kotlin/tests/aockt/y2023/Y2023D09Test.kt
@@ -6,16 +6,17 @@ import java.math.BigInteger
 
 @AdventDay(2023, 9, "Mirage Maintenance")
 class Y2023D09Test : AdventSpec<Y2023D09>({
-
-    partOne {
-        """
+    val exampleInput = """
             0 3 6 9 12 15
             1 3 6 10 15 21
             10 13 16 21 30 45
-        """.trimIndent() shouldOutput 114
+    """.trimIndent()
+
+    partOne {
+        exampleInput shouldOutput 114
     }
 
     partTwo {
+        exampleInput shouldOutput 2
     }
-
 })
