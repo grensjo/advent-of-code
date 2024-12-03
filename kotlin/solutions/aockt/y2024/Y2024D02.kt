af2bc42799d5f83522b8dc9884844b8dcf7be869
Anton Grensjö <anton@grensjo.se>
2024-12-03 23:00:25 +0100

    Clean up 2024 day 2.

--- kotlin/solutions/aockt/y2024/Y2024D02.kt
+++ kotlin/solutions/aockt/y2024/Y2024D02.kt
@@ -4,17 +4,28 @@ import io.github.jadarma.aockt.core.Solution
 import kotlin.math.absoluteValue
 
 object Y2024D02 : Solution {
+    private fun String.split() : List<String> = split("""\s+""".toRegex())
+
+    private fun parseInput(input: String): List<List<Long>> =
+        input.lines().map { line -> line.trim().split().map(String::toLong) }
+
+    private fun List<Long>.isMonotone() : Boolean {
+        val pairs = zipWithNext()
+        return pairs.all { (a,b) -> a >= b } || pairs.all { (a,b) -> a <= b }
+    }
+
+    private fun List<Long>.isSafe() =
+        isMonotone() &&
+                zipWithNext().map { (a, b) -> (a - b).absoluteValue }.all { it in 1..3 }
+
+    private fun List<Long>.isSafeWithDampener() =
+        indices.toList().map { slice(0..<it) + slice((it+1)..<size) }.any { it.isSafe() }
 
     override fun partOne(input: String) =
-        input.to2DLongList().count { it.isSafe() }.also {println(it)}
+        parseInput(input).count { it.isSafe() }
+            .also {println(it)}
 
     override fun partTwo(input: String) =
-        input.to2DLongList().count { it.isSafeWithDampener() }.also {println(it)}
-
-    private fun String.to2DLongList() = lines().map { it.trim().split("\\s+".toRegex()).map { it.toLong() } }
-
-    private fun List<Long>.isMonotone() = zipWithNext().all { (a,b) -> a > b } || zipWithNext().all { (a,b) -> a < b }
-    private fun List<Long>.isSafe() = isMonotone() && zipWithNext().map { (a, b) -> (a - b).absoluteValue }.all { it in 1..3 }
-    private fun List<Long>.isSafeWithDampener() =
-        (0..<size).toList().map {subList(0, it) + subList(it+1, size)}.any { it.isSafe() }
+        parseInput(input).count { it.isSafeWithDampener() }
+            .also {println(it)}
 }
--- kotlin/tests/aockt/y2024/Y2024D02Test.kt
+++ kotlin/tests/aockt/y2024/Y2024D02Test.kt
@@ -3,29 +3,22 @@ package aockt.y2024
 import io.github.jadarma.aockt.test.AdventDay
 import io.github.jadarma.aockt.test.AdventSpec
 
-@AdventDay(2024, 2, "TODO")
+@AdventDay(2024, 2, "Red-Nosed Reports")
 class Y2024D02Test : AdventSpec<Y2024D02>({
-
-    partOne {
-        """
+    val exampleInput = """
             7 6 4 2 1
             1 2 7 8 9
             9 7 6 2 1
             1 3 2 4 5
             8 6 4 4 1
             1 3 6 7 9
-        """.trimIndent() shouldOutput 2
+        """.trimIndent()
+
+    partOne {
+        exampleInput shouldOutput 2
     }
 
     partTwo {
-        """
-            7 6 4 2 1
-            1 2 7 8 9
-            9 7 6 2 1
-            1 3 2 4 5
-            8 6 4 4 1
-            1 3 6 7 9
-        """.trimIndent() shouldOutput 4
+        exampleInput shouldOutput 4
     }
-
 })
