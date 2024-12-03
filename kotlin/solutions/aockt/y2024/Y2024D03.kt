d6566124319a6d040e55d9907eadd5eb5c2dee01
Anton Grensjö <anton@grensjo.se>
2024-12-03 23:08:58 +0100

    2024 day 3 part 2.

--- kotlin/solutions/aockt/y2024/Y2024D03.kt
+++ kotlin/solutions/aockt/y2024/Y2024D03.kt
@@ -3,21 +3,50 @@ package aockt.y2024
 import io.github.jadarma.aockt.core.Solution
 
 object Y2024D03 : Solution {
-    val valid_regex = """mul\(\d{1,3},\d{1,3}\)""".toRegex()
+    private val valid_regex = """mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)""".toRegex()
 
-    private fun String.solve() =
-        valid_regex.findAll(this).toList().map { it.value }.sumOf { it.mul() }
+    sealed class Symbol
+    data class Mul(val a: Long, val b: Long) : Symbol() {
+        fun product() = a * b
+    }
+    data object Enable : Symbol()
+    data object Disable : Symbol()
 
-    private fun String.mul(): Long {
-       val (a_raw, b_raw) = this.split(",")
+    private fun String.toSymbol(): Symbol =
+        when {
+            this.startsWith("mul") -> this.toMul()
+            this == "do()" -> Enable
+            this == "don't()" -> Disable
+            else -> throw IllegalArgumentException()
+        }
+
+    private fun String.toMul(): Mul {
+        val (aRaw, bRaw) = this.split(",")
         assert(this.startsWith("mul("))
         assert(this.endsWith(")"))
-        val a = a_raw.substringAfter("(").toLong()
-        val b = b_raw.substringBefore(")").toLong()
-        return a*b
+        val a = aRaw.substringAfter("(").toLong()
+        val b = bRaw.substringBefore(")").toLong()
+        return Mul(a, b)
     }
 
-    override fun partOne(input: String) = input.solve().also { println(it) }
+    private fun List<Symbol>.execute(withConditionals: Boolean) : Long {
+        var enabled = true
+        var sum: Long = 0
 
-//    override fun partTwo(input: String) = input.length
+        for (symbol in this) {
+            when (symbol) {
+                Enable -> enabled = true
+                Disable -> if (withConditionals) { enabled = false }
+                is Mul -> if (enabled) { sum += symbol.product() }
+            }
+        }
+
+        return sum
+    }
+
+    private fun String.solve(withConditionals: Boolean = false) =
+        valid_regex.findAll(this).toList().map { it.value.toSymbol() }.execute(withConditionals)
+
+    override fun partOne(input: String) = input.solve(withConditionals = false).also { println(it) }
+    override fun partTwo(input: String) = input.solve(withConditionals = true).also { println(it) }
 }
