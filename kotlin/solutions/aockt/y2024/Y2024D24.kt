c256aecd8a2cf94302dee95fe7f09a98e6df7056
Anton Grensjö <anton@grensjo.se>
2024-12-25 03:50:30 +0100

    Clean up 2024 day 24 part 2 a bit.

--- kotlin/solutions/aockt/y2024/Y2024D24.kt
+++ kotlin/solutions/aockt/y2024/Y2024D24.kt
@@ -2,7 +2,6 @@ package aockt.y2024
 
 import aockt.y2024.Y2024D24.Operation.*
 import io.github.jadarma.aockt.core.Solution
-import kotlin.math.exp
 import kotlin.math.max
 import kotlin.random.Random
 
@@ -80,26 +79,16 @@ object Y2024D24 : Solution {
             current, acc -> ((acc shl 1) or current.toLong())
         }
 
-    private fun extractIncorrectOutputs(label: Char, actual: Long, expected: Long): Set<String> =
+    private fun extractIncorrectOutputs(actual: Long, expected: Long): Set<String> =
         buildSet {
             var i = 0
             while (1L shl i < max(expected, actual)) {
                 if ((actual and (1L shl i)) != (expected and (1L shl i))) {
-                    add("${label}${i.toString().padStart(2, '0')}")
+                    add("z${i.toString().padStart(2, '0')}")
                 }
                 i++
             }
         }
-
-    override fun partOne(input: String): Long {
-        val (stateStr, gatesStr) = input.split("\n\n")
-        val state = parseState(stateStr)
-        val gates = parseGates(gatesStr)
-        println(state.size)
-        println(gates.size)
-
-        return solve(state, gates)
-    }
 
     private fun swapGates(gates: MutableMap<String, Gate>, label1: String, label2: String) {
         val gate1 = gates[label2]!!
@@ -110,39 +99,45 @@ object Y2024D24 : Solution {
         gates[label2] = gate2
     }
 
+    override fun partOne(input: String): Long {
+        val (stateStr, gatesStr) = input.split("\n\n")
+        val state = parseState(stateStr)
+        val gates = parseGates(gatesStr)
+
+        return solve(state, gates)
+    }
+
     override fun partTwo(input: String): String {
         val (stateStr, gatesStr) = input.split("\n\n")
         val inputState = parseState(stateStr)
         val gates = parseGates(gatesStr).toMutableMap()
-        println(inputState.size)
-        println(gates.size)
-
         val numInputBits = inputState.size / 2
-        val isExample = input.lines().size == 19
 
-        fun getIncorrectOutputs(x: Long, y: Long, isExample: Boolean): Set<String> {
+        fun getIncorrectOutputs(x: Long, y: Long): Set<String> {
             val inputMap = x.toInputMap('x', numInputBits) + y.toInputMap('y', numInputBits)
             val result = solve(inputMap, gates)
-            val expected = if (isExample) { x and y } else { x + y }
-            val incorrectBits = extractIncorrectOutputs('z', result, expected).toMutableSet()
+            val expected =  x + y
+            val incorrectBits = extractIncorrectOutputs(result, expected).toMutableSet()
             return incorrectBits
         }
 
-
-        val incorrectBits: MutableSet<String> = mutableSetOf()
-
-        fun findAncestors(label: String, ancestorSet: MutableSet<String>) {
-            ancestorSet.add(label)
-            if (label in gates) {
-                val gate = gates[label]!!
-                findAncestors(gate.inWire1, ancestorSet)
-                findAncestors(gate.inWire2, ancestorSet)
+        fun printIncorrectOutputsFromManyTestRuns() {
+            val incorrectBits: MutableSet<String> = mutableSetOf()
+            for (i in 0..20000) {
+                val x = Random.nextLong(1L shl numInputBits)
+                val y = Random.nextLong(1L shl numInputBits)
+                incorrectBits += getIncorrectOutputs(x, y)
             }
+
+            println("${incorrectBits.size} output bits are incorrect, " +
+                    "The first incorrect one is ${incorrectBits.minOrNull() ?: "N/A"}.")
         }
 
-        if (isExample) { return "" }
+        printIncorrectOutputsFromManyTestRuns()
 
-        // Swap z12 and qdg
+        // We will iteratively find the output labels to swap. The method call above will get you the first incorrect
+        // one. Inspect the input file manually to figure out which to swap. Add it to the list below and run the
+        // program again.
         val toSwap = listOf(
             "z12" to "qdg",
             "z19" to "vvf",
@@ -152,34 +147,8 @@ object Y2024D24 : Solution {
         for ((a, b) in toSwap) {
             swapGates(gates, a, b)
         }
-//        swapGates(gates, "z12", "qdg")
-//        swapGates(gates, "z19", "vvf")
-//        swapGates(gates, "fgn", "dck")
-//        swapGates(gates, "z37", "nvh")
 
-        println("Trying again.")
-        for (i in 0..37) {
-            val label = "z${i.toString().padStart(2, '0')}"
-            val ancestorSet: MutableSet<String> = mutableSetOf()
-            findAncestors(label, ancestorSet)
-            println("$label: ${ancestorSet.sorted()}")
-        }
-
-        // Start with testing the input state
-        val x = inputState.extractLong('x')
-        val y = inputState.extractLong('y')
-        incorrectBits += getIncorrectOutputs(x, y, isExample)
-        println(incorrectBits)
-
-        for (i in 0..20000) {
-            val x = Random.nextLong(1L shl numInputBits)
-            val y = Random.nextLong(1L shl numInputBits)
-            incorrectBits += getIncorrectOutputs(x, y, isExample)
-            if (i % 10000 == 0) {
-                println("$i: num wrong = ${incorrectBits.size}")
-            }
-        }
-        println(incorrectBits.sorted())
+        printIncorrectOutputsFromManyTestRuns()
 
         return toSwap.flatMap { (a, b) -> listOf(a, b) }.sorted().joinToString(",").also { println(it) }
     }
