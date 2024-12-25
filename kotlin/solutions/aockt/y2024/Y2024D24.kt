0b99c19de4ce64c30cc05b637569c89fae862736
Anton Grensjö <anton@grensjo.se>
2024-12-25 03:35:46 +0100

    2024 day 24 not cleaned up.

--- kotlin/solutions/aockt/y2024/Y2024D24.kt
+++ kotlin/solutions/aockt/y2024/Y2024D24.kt
@@ -2,7 +2,9 @@ package aockt.y2024
 
 import aockt.y2024.Y2024D24.Operation.*
 import io.github.jadarma.aockt.core.Solution
-import java.text.ParseException
+import kotlin.math.exp
+import kotlin.math.max
+import kotlin.random.Random
 
 object Y2024D24 : Solution {
 
@@ -12,7 +14,7 @@ object Y2024D24 : Solution {
         XOR,
     }
 
-    private data class Gate(val inWire1: String, val op: Operation, val inWire2: String, val outWire: String) {
+    private data class Gate(val inWire1: String, val op: Operation, val inWire2: String, var outWire: String) {
         fun getOutput(state: MutableMap<String, Boolean>, gates: Map<String, Gate>): Boolean {
             state[outWire]?.also { return it }
 
@@ -51,13 +53,8 @@ object Y2024D24 : Solution {
         false -> 0L
     }
 
-    private fun solve(input: String): Long {
-        val (inputState, inputGates) = input.split("\n\n")
-        val state = parseState(inputState).toMutableMap()
-        val gates = parseGates(inputGates)
-        println(state.size)
-        println(gates.size)
-
+    private fun solve(inputState: Map<String, Boolean>, gates: Map<String, Gate>): Long {
+        val state = inputState.toMutableMap()
         val outputs = gates.values.map { it.outWire }.filter { it.startsWith('z') }.sortedDescending()
 
         var result = 0L
@@ -66,14 +63,124 @@ object Y2024D24 : Solution {
             result = result or (gates[outputWire]?.getOutput(state, gates)?.toLong() ?: throw IllegalStateException())
         }
 
-        return result.also { println(it) }
+        return result
     }
 
-    override fun partOne(input: String) = solve(input)
+    private fun Long.toInputMap(label: Char, nBits: Int): Map<String, Boolean> =
+        buildMap {
+            var tmp = this@toInputMap
+            for (i in 0..<nBits) {
+                put("${label}${i.toString().padStart(2, '0')}", (tmp and 1L) == 1L)
+                tmp = tmp shr 1
+            }
+        }
 
-//    override fun partTwo(input: String): Long {
-//        println(7L.toInputMap('x'))
-//        println(7L.toInputMap('x').extractLong('x'))
-//        return 0L
-//    }
+    private fun Map<String, Boolean>.extractLong(label: Char) =
+        filter { it.key.startsWith(label) }.toList().sortedBy{ it.first }.map { it.second}.foldRight(0L) {
+            current, acc -> ((acc shl 1) or current.toLong())
+        }
+
+    private fun extractIncorrectOutputs(label: Char, actual: Long, expected: Long): Set<String> =
+        buildSet {
+            var i = 0
+            while (1L shl i < max(expected, actual)) {
+                if ((actual and (1L shl i)) != (expected and (1L shl i))) {
+                    add("${label}${i.toString().padStart(2, '0')}")
+                }
+                i++
+            }
+        }
+
+    override fun partOne(input: String): Long {
+        val (stateStr, gatesStr) = input.split("\n\n")
+        val state = parseState(stateStr)
+        val gates = parseGates(gatesStr)
+        println(state.size)
+        println(gates.size)
+
+        return solve(state, gates)
+    }
+
+    private fun swapGates(gates: MutableMap<String, Gate>, label1: String, label2: String) {
+        val gate1 = gates[label2]!!
+        val gate2 = gates[label1]!!
+        gate1.outWire = label1
+        gate2.outWire = label2
+        gates[label1] = gate1
+        gates[label2] = gate2
+    }
+
+    override fun partTwo(input: String): String {
+        val (stateStr, gatesStr) = input.split("\n\n")
+        val inputState = parseState(stateStr)
+        val gates = parseGates(gatesStr).toMutableMap()
+        println(inputState.size)
+        println(gates.size)
+
+        val numInputBits = inputState.size / 2
+        val isExample = input.lines().size == 19
+
+        fun getIncorrectOutputs(x: Long, y: Long, isExample: Boolean): Set<String> {
+            val inputMap = x.toInputMap('x', numInputBits) + y.toInputMap('y', numInputBits)
+            val result = solve(inputMap, gates)
+            val expected = if (isExample) { x and y } else { x + y }
+            val incorrectBits = extractIncorrectOutputs('z', result, expected).toMutableSet()
+            return incorrectBits
+        }
+
+
+        val incorrectBits: MutableSet<String> = mutableSetOf()
+
+        fun findAncestors(label: String, ancestorSet: MutableSet<String>) {
+            ancestorSet.add(label)
+            if (label in gates) {
+                val gate = gates[label]!!
+                findAncestors(gate.inWire1, ancestorSet)
+                findAncestors(gate.inWire2, ancestorSet)
+            }
+        }
+
+        if (isExample) { return "" }
+
+        // Swap z12 and qdg
+        val toSwap = listOf(
+            "z12" to "qdg",
+            "z19" to "vvf",
+            "fgn" to "dck",
+            "z37" to "nvh",
+        )
+        for ((a, b) in toSwap) {
+            swapGates(gates, a, b)
+        }
+//        swapGates(gates, "z12", "qdg")
+//        swapGates(gates, "z19", "vvf")
+//        swapGates(gates, "fgn", "dck")
+//        swapGates(gates, "z37", "nvh")
+
+        println("Trying again.")
+        for (i in 0..37) {
+            val label = "z${i.toString().padStart(2, '0')}"
+            val ancestorSet: MutableSet<String> = mutableSetOf()
+            findAncestors(label, ancestorSet)
+            println("$label: ${ancestorSet.sorted()}")
+        }
+
+        // Start with testing the input state
+        val x = inputState.extractLong('x')
+        val y = inputState.extractLong('y')
+        incorrectBits += getIncorrectOutputs(x, y, isExample)
+        println(incorrectBits)
+
+        for (i in 0..20000) {
+            val x = Random.nextLong(1L shl numInputBits)
+            val y = Random.nextLong(1L shl numInputBits)
+            incorrectBits += getIncorrectOutputs(x, y, isExample)
+            if (i % 10000 == 0) {
+                println("$i: num wrong = ${incorrectBits.size}")
+            }
+        }
+        println(incorrectBits.sorted())
+
+        return toSwap.flatMap { (a, b) -> listOf(a, b) }.sorted().joinToString(",").also { println(it) }
+    }
 }
